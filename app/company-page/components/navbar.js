59a7dd608fe710184b7ae88374b59914a36e2c88
Jiez Tee <jieztee@gmail.com>
2024-04-01 21:58:50 +0800

    改了什么，又好像什么都没改

--- app/company-page/components/navbar.js
+++ app/company-page/components/navbar.js
@@ -27,7 +27,7 @@ export default function Navbar({brandName, navigationItems = []}){
     return(
         <>
             <div className="navbar bg-base-100 sticky top-0 z-50 lg:static lg:z-0">
-                <div className="navbar-start">
+                <div className="flex-initial">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -38,7 +38,7 @@ export default function Navbar({brandName, navigationItems = []}){
                     </div>
                     <Link className="btn btn-ghost text-xl" href="/company-page">{brandName}</Link>
                 </div>
-                <div className="navbar-end hidden lg:flex">
+                <div className="flex-1 justify-end hidden lg:flex">
                     <ul className="menu menu-horizontal px-1 gap-4">
                         <NavBarList navigationItems={navigationItems}/>
                     </ul>
