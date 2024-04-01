8125822ceda4e705a661cf47184e44d7304575af
Jiez Tee <jieztee@gmail.com>
2024-04-01 21:32:40 +0800

    themeChanger on navbar

--- app/company-page/components/navbar.js
+++ app/company-page/components/navbar.js
@@ -6,6 +6,22 @@
  */
 
 import Link from 'next/link';
+import ThemeChanger from  './themeChanger';
+
+function NavBarList({navigationItems}){
+    return(
+        <>
+            {navigationItems.map((item, index)=>(
+                <li key={index}>
+                    <Link href={`${item.link}`}>{item.name}</Link>
+                </li>
+            ))}
+            <li>
+                <ThemeChanger/>
+            </li>
+        </>
+    )
+}
 
 export default function Navbar({brandName, navigationItems = []}){
     return(
@@ -17,22 +33,14 @@ export default function Navbar({brandName, navigationItems = []}){
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            {navigationItems.map((item, index)=>(
-                                <li key={index}>
-                                    <Link href={`${item.link}`}>{item.name}</Link>
-                                </li>
-                            ))}
+                            <NavBarList navigationItems={navigationItems}/>
                         </ul>
                     </div>
                     <Link className="btn btn-ghost text-xl" href="/company-page">{brandName}</Link>
                 </div>
                 <div className="navbar-end hidden lg:flex">
                     <ul className="menu menu-horizontal px-1 gap-4">
-                        {navigationItems.map((item, index)=>(
-                            <li key={index}>
-                                <a href={`${item.link}`}>{item.name}</a>
-                            </li>
-                        ))}
+                        <NavBarList navigationItems={navigationItems}/>
                     </ul>
                 </div>
             </div>
