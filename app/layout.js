1fbdfc30bbada50be45b97cb186779c78ec11064
Jiez Tee <jieztee@gmail.com>
2024-04-04 02:06:44 +0800

    change light theme

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="winter">
+    <html lang="en" data-theme="winter" className="scroll-smooth">
       <body className={inter.className}>{children}</body>
     </html>
   );
