9a23e9b6fc41be7ba4401ec1769b848cf0df5a5d
Jiez Tee <jieztee@gmail.com>
2024-04-03 23:35:30 +0800

    animations on tabs

--- app/company-page/components/tabs.js
+++ app/company-page/components/tabs.js
@@ -4,7 +4,7 @@ export default function Tabs({items}){
     return(
         <div role="tablist" className="hidden lg:tabs tabs-boxed sticky top-0">
             {items.map((item, index)=>(
-                <Link key={index} href={`${item.link}`} className="tab">{item.name}</Link>
+                <Link key={index} href={`${item.link}`} className="tab hover:scale-110 hover:bg-base-300 hover:text-base-content duration-300">{item.name}</Link>
             ))}
         </div>
     )
