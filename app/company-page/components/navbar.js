497bdde8cf57599934ec7b67364523f5a11582cc
Jiez Tee <jieztee@gmail.com>
2024-04-04 02:05:45 +0800

    add shadow

--- app/company-page/components/navbar.js
+++ app/company-page/components/navbar.js
@@ -26,7 +26,7 @@ function NavBarList({navigationItems}){
 export default function Navbar({brandName, navigationItems = []}){
     return(
         <>
-            <div className="navbar bg-base-100 sticky top-0 z-50 lg:static lg:z-0">
+            <div className="navbar bg-base-100 sticky top-0 z-50 shadow-lg">
                 <div className="flex-initial">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
