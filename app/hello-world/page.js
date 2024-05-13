7b9bb3667b563de9fd2c6d4d0069acf1daa41d23
Jiez Tee <jieztee@gmail.com>
2024-05-13 18:13:25 +0800

    update hello world

---
+++ app/hello-world/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-yellow-400 to-orange-500">
+        <h1 className="text-5xl font-bold text-white text-shadow-lg">
+          Hello World
+        </h1>
+      </div>
+    );
+  }
