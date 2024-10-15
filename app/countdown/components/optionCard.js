cf3cff93e42daa52c9d2ce34d85ad7679771ed8b
Jiez Tee <jieztee@gmail.com>
2024-10-15 08:21:07 +0800

    Refactor .gitignore and add .vercel

--- app/countdown/components/optionCard.js
+++ app/countdown/components/optionCard.js
@@ -1,9 +1,9 @@
 // Create a card that will display the date and festival name
 // Use the date and festival name from the dateDay prop
 
-export default function OptionCard({dateDay}){
+export default function OptionCard({dateDay, clickHandler}) {
     return(
-        <div className="card">
+        <div className="card shadow-xl w-80" onClick={clickHandler()}>
             <div className="card-body">
                 <h2 className="card-title">
                     {dateDay.festivalName}
--- app/countdown/page.js
+++ app/countdown/page.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 import CountdownTimer from './components/countdown-timer';
 import OptionCard from './components/optionCard';
 import { sql } from "@vercel/postgres";
 
 export default async function CountdownPage() {
   const result = await sql`SELECT * FROM public."Festival";`;
+
   const dateOptions = result.rows;
+
   const dateOptionsWithShortDate = dateOptions.map(option => {
     const festivalDate = new Date(option.festivalDate);
     festivalDate.setHours(festivalDate.getHours() + 8); // Convert to GMT +8
@@ -15,11 +18,19 @@ export default async function CountdownPage() {
       festivalDate: shortDate
     };
   });
+  
+  // go to festival page when clicked
+  const clickHandler = ({festivalName}) => {
+    Link.push(`/${festivalName}`);
+  };
+
   return (
     <>
-      {dateOptionsWithShortDate.map(dateDay => (
-        <OptionCard key={dateDay.festivalName} dateDay={dateDay} />
-      ))}
+      <div className='flex flex-row gap-4 m-6'>
+        {dateOptionsWithShortDate.map(dateDay => (
+          <OptionCard key={dateDay.festivalName} dateDay={dateDay} clickHandler={clickHandler}/>
+        ))}
+      </div>
       <CountdownTimer targetDate="2025-01-01"/>
     </>
   );
