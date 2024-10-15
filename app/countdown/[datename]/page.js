1c3ba9706a766fc1b7fd64da3c93bc426cddc702
Jiez Tee <jieztee@gmail.com>
2024-10-15 13:28:46 +0800

    Refactor page.js to add moment-timezone and @vercel/postgres dependencies

--- app/countdown/[datename]/page.js
+++ app/countdown/[datename]/page.js
@@ -1,8 +1,19 @@
-export default function DateNamePage({params}){
+import { sql } from "@vercel/postgres";
+import moment from 'moment-timezone';
+import CountdownTimer from "../components/countdown-timer";
+
+export default async function DateNamePage({params}){
     const dateName = decodeURIComponent(params.datename);
+    const dateQuery = await sql`SELECT "festivalDate" FROM public."Festival" WHERE "festivalName" = ${dateName};`;
+    if (dateQuery.rows.length === 0) {
+        const message = `No festival found with the name ${dateName}`;
+    }
+    const festivalDate = moment(dateQuery.rows[0].festivalDate).tz('Asia/Singapore');
+    const shortDate = festivalDate.format('YYYY-MM-DD');
     return (
         <>
             {dateName}
+            <CountdownTimer targetDate={shortDate}/>
         </>
     )
 }
--- app/countdown/page.js
+++ app/countdown/page.js
@@ -1,37 +1,32 @@
-import React from 'react';
-import Link from 'next/link';
 import CountdownTimer from './components/countdown-timer';
 import OptionCard from './components/optionCard';
 import { sql } from "@vercel/postgres";
+import moment from 'moment-timezone'
 
 export default async function CountdownPage() {
-  const result = await sql`SELECT * FROM public."Festival";`;
+  const result = await sql`SELECT * FROM public."Festival" ORDER BY "festivalDate" ASC;`;
 
   const dateOptions = result.rows;
 
   const dateOptionsWithShortDate = dateOptions.map(option => {
-    const festivalDate = new Date(option.festivalDate);
-    festivalDate.setHours(festivalDate.getHours() + 8); // Convert to GMT +8
-    const shortDate = festivalDate.toISOString().split('T')[0]; // Convert to short date string
-    return {
+  const festivalDate = moment(option.festivalDate).tz('Asia/Singapore');
+  const shortDate = festivalDate.format('DD MMM YYYY');
+  return {
       ...option,
       festivalDate: shortDate
     };
   });
-  
-  // go to festival page when clicked
-  const clickHandler = ({festivalName}) => {
-    Link.push(`/${festivalName}`);
-  };
 
   return (
-    <>
-      <div className='flex flex-row gap-4 m-6'>
+    <div className='flex flex-col items-center min-h-screen'>
+      <div className='flex flex-wrap gap-4 m-6'>
         {dateOptionsWithShortDate.map(dateDay => (
-          <OptionCard key={dateDay.festivalName} dateDay={dateDay} clickHandler={clickHandler}/>
+          <OptionCard key={dateDay.festivalName} dateDay={dateDay}/>
         ))}
       </div>
-      <CountdownTimer targetDate="2025-01-01"/>
-    </>
+      <div className='flex-grow flex items-center justify-center'>
+        <CountdownTimer targetDate="2025-01-01"/>
+      </div>
+    </div>
   );
   }
