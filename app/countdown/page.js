01aa669935e5679ecf1994883ee69b8de91efcff
Jiez Tee <jieztee@gmail.com>
2025-05-23 00:13:17 +0800

    Fix timezone handling in SQL query and update layout for CountdownPage

--- app/countdown/page.js
+++ app/countdown/page.js
@@ -3,10 +3,12 @@ import { sql } from "@vercel/postgres";
 import moment from 'moment-timezone';
 import CustomCard from './components/customCard';
 
+export const dynamic = 'force-dynamic';
+
 export default async function CountdownPage() {
   let result;
   try {
-    result = await sql`SELECT * FROM public."Festival" WHERE "festivalDate" > NOW() ORDER BY "festivalDate" ASC LIMIT 6;`;
+    result = await sql`SELECT * FROM public."Festival" WHERE "festivalDate" > timezone('Asia/Singapore', NOW()) ORDER BY "festivalDate" ASC LIMIT 6;`;
   } catch (error) {
     console.error('Error executing SQL query:', error);
     result = { rows: [] }; // Fallback to an empty array if there's an error
@@ -26,7 +28,7 @@ export default async function CountdownPage() {
 
   return (
     <div className='flex flex-col items-center min-h-screen'>
-      <div className='order-2 mb-6 lg:order-1 flex flex-row flex-wrap justify-center gap-4 lg:mt-6'>
+      <div className='order-2 mb-6 lg:order-1 lg:mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {dateOptionsWithShortDate.map(dateDay => (
           <OptionCard key={dateDay.festivalName} dateDay={dateDay}/>
         ))}
